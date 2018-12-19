217b8efd731267ba49c3b730153aa1e392538689
[fandong] <[fandong@qq.com]>
2018-12-19 23:12:41 +0800

    update

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,16 +2,16 @@
 # coding=utf-8
 from flask import Flask
 import os
-from flaskr import db,auth,blog
+from flaskr import db, auth, blog
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path,'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
     if test_config is None:
-        app.config.from_pyfile('config.py',silent=True)
+        app.config.from_pyfile('config.py', silent=True)
     else:
         app.config.from_mapping(test_config)
 
@@ -29,6 +29,6 @@ def create_app(test_config=None):
     app.register_blueprint(auth.bp)
 
     app.register_blueprint(blog.bp)
-    app.add_url_rule('/',endpoint='index')
+    app.add_url_rule('/', endpoint='index')
 
     return app
--- flaskr/auth.py
+++ flaskr/auth.py
@@ -13,6 +13,8 @@ def register():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        gender = request.form['gender']
+        school = request.form['school']
         db = get_db()
         error = None
 
@@ -20,11 +22,15 @@ def register():
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
+        elif not gender:
+            error = 'Gender is required.'
+        elif not school:
+            error = 'School is required.'
         elif db.execute('SELECT id FROM user WHERE username = ?',(username,)).fetchone() is not None:
             error = 'User {} is already registered.'.format(username)
 
         if error is None:
-            db.execute('INSERT INTO user (username,password) VALUES (?,?)',(username,generate_password_hash(password)))
+            db.execute('INSERT INTO user (username,password, gender, school) VALUES (?,?,?,?)', (username,generate_password_hash(password), gender, school))
             db.commit()
             return redirect(url_for('auth.login'))
 
@@ -39,11 +45,11 @@ def login():
         password = request.form['password']
         db = get_db()
         error = None
-        user = db.execute('SELECT * FROM user WHERE username = ?',(username,)).fetchone()
+        user = db.execute('SELECT * FROM user WHERE username = ?', (username,)).fetchone()
 
         if user is None:
             error = 'Incorrect username.'
-        elif not check_password_hash(user['password'],password):
+        elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
 
         if error is None:
@@ -63,7 +69,7 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?',(user_id,)).fetchone()
+            'SELECT * FROM user WHERE id = ?', (user_id,)).fetchone()
             
 @bp.route('/logout')
 def logout():
